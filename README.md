680ff81f438362a0c390be736f40827b332ab5d5
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-16 20:56:16 -0500

    rules added to README, html skeleton code added, app.js logic beginning

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PigDiceGame
+
+Game Rules:
+
+There are 2 players.  A player begins by rolling the die.  If the die is any number that is not "ONE" then that amount is added to their score.  If, however, a "ONE" is rolled, the player's score is reset to 0 and the next player's turn begins.
+
+However, a player is able to "HOLD" their score by forfeitting their turn to the other player.
+
+The first player to reach a score of 100 wins!
---
+++ app.js
@@ -0,0 +1,16 @@
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(game) {
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        var diceDisplay = document.querySelector('.dice');
+        diceDisplay.style.display = 'block';
+        diceDisplay.src = 'dice-' + dice + '.png';
+
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-' + currentPlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
+    }    
+});
